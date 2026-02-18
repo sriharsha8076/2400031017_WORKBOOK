cca253b6f2ee337cb9ca776f880c05c12edcbfe2
sriharsha8076 <2400031017@kluniversity.in>
2026-02-18 10:02:05 +0530

    First commit

---
+++ src/main/java/com/klu/service/MessageService.java
@@ -0,0 +1,10 @@
+package com.klu.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageService {
+	public String getMessage() {
+		return "Welcome to Spring Boot Application";
+	}
+}
